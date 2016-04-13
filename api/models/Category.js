07555aea7f472013d31dc3a15be650ffd77ef47b
Álvaro Saco <alvarosaco@gmail.com>
2016-04-13 19:05:00 +0200

    Añadido el modelo Category

---
+++ api/models/Category.js
@@ -0,0 +1,29 @@
+/**
+* Category.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+    name: {
+      type: 'string'
+    },
+    description: {
+      type: 'boolean'
+    },
+    slug: {
+      type: 'string'
+    },
+
+    // Add a reference to Compartios
+    compartios: {
+      collection: 'compartio',
+      via: 'category'
+    }
+
+
+  }
+};
