7e4102eb7e1ea8ecbb2074080dd3ddc0f38ed4ed
Álvaro Saco <alvarosaco@gmail.com>
2016-04-13 19:04:45 +0200

    Añadido el modelo "City"

---
+++ api/models/City.js
@@ -0,0 +1,26 @@
+/**
+* City.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+    name: {
+      type: 'string'
+    },
+    slug: {
+      type: 'string'
+    },
+    is_active: {
+      type: 'boolean'
+    },
+    // Add a reference to Compartios
+    compartios: {
+      collection: 'compartio',
+      via: 'city'
+    }
+  }
+};
