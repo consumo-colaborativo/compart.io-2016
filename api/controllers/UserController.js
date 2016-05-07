11452819e0f9dea583c96a3aa409c45a496e3cf3
Álvaro Saco <alvarosaco@gmail.com>
2016-05-07 22:42:07 +0200

    Añado prueba de validación en el modelo User

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -6,6 +6,30 @@
  */
 
 module.exports = {
-	
-};
+	signup: function(req, res) {
+    if (_.isUndefined(req.param('email'))) {
+      return res.badRequest('An email address is required!');
+		}
 
+		if (_.isUndefined(req.param('password'))) {
+    	return res.badRequest('A password is required!');
+		}
+
+		if (req.param('password').length < 6) {
+    	return res.badRequest('Password must be at least 6 characters!');
+  	}
+
+		if (_.isUndefined(req.param('username'))) {
+    	return res.badRequest('A username is required!');
+		}
+
+		var user = {
+    	email: req.param('email'),
+			username: req.param('username'),
+			password: req.param('password')
+		};
+
+		return res.json(user);
+		}
+		
+};
